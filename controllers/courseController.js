f2aa1bd7aac356c4f855c6be8b6b163d0b38259e
Tapiwanashe Kanda <tapiwanashekanda@Tapiwanashes-MacBook-Air.local>
2024-02-28 19:51:58 +0200

    added few comments on course controller

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,38 +1,32 @@
 const Course = require('../models/course')
-
+// add new course function
 const createCourses = async (req, res) =>{
     const title = req.body.title 
     const description = req.body.description
     const hours_done = req.body.hours_done
     const total_hours = req.body.total_hours
-
     const course = await Course.create({title, description, hours_done, total_hours})
-
     res.json({course: course})
 }
-
+// get courses function
 const fetchCourses = async (req, res) =>{
     const courses = await Course.find()
     res.json({courses: courses})
 }
-
+//search course function
 const fetchCourse = async (req, res) =>{
     const course = await Course.findById(req.params.id)
     res.json({course: course})
 }
-
+//edit course function
 const updateCourse = async (req, res) =>{
-
     // update course by id in the db
     await Course.findByIdAndUpdate(req.params.id, req.body)
-
     // fetch and store update course
     const course = await Course.findById(req.params.id)
-
-
     res.json({course: course})
 }
-
+// delete course function
 const deleteCourse = async (req, res) =>{
     await Course.findByIdAndDelete(req.params.id)
     res.json({message: "Course deleted"})
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 if (process.env.NODE_ENV != "production"){
     require('dotenv').config()
 }
-
 // import dependencies
 const express = require('express')
 const db = require('./config/db')
@@ -11,13 +10,11 @@ const cors = require('cors')
 
 //create express app
 const app = express()
-
 //configure express app to handle json requests, cors
 app.use(express.json())
 app.use(cors())
 //connect to database
 db()
-
 //api routes
 app.post("/courses", courseController.createCourses)
 //route to get all courses
@@ -28,6 +25,5 @@ app.get("/courses/:id",courseController.fetchCourse )
 app.put("/courses/:id", courseController.updateCourse)
 // route to delete a course by id
 app.delete("/courses/:id", courseController.deleteCourse)
-
 //listening port
 app.listen(process.env.PORT)
